d137e35d1fa348fdfb7d6bae2123cb40818d6a3f
Kaz Kylheku <kkylheku@gmail.com>
2009-11-09 22:25:39 -0800

    New C++ utilities in dict.h. Documented in docs.ltx. Tabs eliminated from docs.ltx.

--- dict.h
+++ dict.h
@@ -147,6 +147,336 @@ extern void dict_merge(dict_t *, dict_t *);
 
 #ifdef __cplusplus
 }
+
+#include <functional>
+
+namespace kazlib
+{
+    class dnode : public dnode_t {
+    public:
+        dnode() { dnode_init(this, 0); }
+        bool is_in_a_dict()
+        {
+            return dnode_is_in_a_dict(this);
+        }
+    };
+
+    template <typename KEY>
+    int default_compare(const KEY &left, const KEY &right)
+    {
+        if (left < right)
+            return -1;
+        else if (left == right)
+            return 0;
+        else
+            return 1;
+    }
+
+    template <
+        class NODE_METHOD,
+        class KEY_METHOD,
+        class COMPARE_METHOD,
+        bool ALLOW_DUPES = false
+    >
+    class dict_base : public dict_t {
+    private:
+        dict_base(const dict_base &);
+        void operator = (const dict_base &);
+    protected:
+        typedef typename KEY_METHOD::KEY_TYPE KEY;
+        typedef typename NODE_METHOD::ITEM_TYPE ITEM;
+
+        dict_base(dictcount_t count)
+        {
+            dict_init(this, count, &COMPARE_METHOD::compare);
+            if (ALLOW_DUPES)
+                dict_allow_dupes(this);
+        }
+        static dnode_t *item2dnode(ITEM *item)
+        {
+            return NODE_METHOD::item2dnode(item);
+        }
+        static ITEM *dnode2item(dnode_t *dnode)
+        {
+            return NODE_METHOD::dnode2item(dnode);
+        }
+        static void *item2key(ITEM *item)
+        {
+            return KEY_METHOD::item2key(item);
+        }
+    public:
+        dictcount_t count() 
+        {
+            return dict_count(this);
+        }
+        void insert(ITEM *pitem)
+        {
+            dict_insert(this, item2dnode(pitem), item2key(pitem));
+        }
+        void insert(ITEM &item)
+        {
+            insert(&item);
+        }
+        void erase(ITEM *pitem)
+        {
+            dict_delete(this, item2dnode(pitem));
+        }
+        void erase(ITEM &item)
+        {
+            erase(&item);
+        }
+        ITEM *lookup(const KEY *pkey)
+        {
+            return dnode2item(dict_lookup(this, pkey));
+        }
+        ITEM *lookup(const KEY &key)
+        {
+            return lookup(&key);
+        }
+        ITEM *upper_bound(const KEY *pkey)
+        {
+            return dnode2item(dict_upper_bound(this, pkey));
+        }
+        ITEM *upper_bound(const KEY &key)
+        {
+            return upper_bound(&key);
+        }
+        ITEM *lower_bound(const KEY *pkey)
+        {
+            return dnode2item(dict_lower_bound(this, pkey));
+        }
+        ITEM *lower_bound(const KEY &key)
+        {
+            return lower_bound(&key);
+        }
+        ITEM *first()
+        {
+            return dnode2item(dict_first(this));
+        }
+        ITEM *last()
+        {
+            return dnode2item(dict_last(this));
+        }
+        ITEM *next(ITEM *pitem)
+        {
+            return dnode2item(dict_next(this, item2dnode(pitem)));
+        }
+        ITEM *next(ITEM &item)
+        {
+            return next(&item);
+        }
+        ITEM *prev(ITEM *pitem)
+        {
+            return dnode2item(dict_prev(this, item2dnode(pitem)));
+        }
+        ITEM *prev(ITEM &item)
+        {
+            return prev(&item);
+        }
+    };
+
+    template <class ITEM, dnode ITEM::* DNODE_OFFSET>
+    class dnode_is_member {
+    private:
+        dnode_is_member(const dnode_is_member &);
+        void operator = (const dnode_is_member &);
+    public:
+        typedef ITEM ITEM_TYPE;
+        static ITEM *dnode2item(dnode_t *node)
+        {
+            if (node == 0)
+                return 0;
+            const ptrdiff_t offset = (char *) &(((ITEM *) 0)->*DNODE_OFFSET)
+                                     - ((char *) 0);
+            return (ITEM *) (((char *) node) - offset);
+        }
+        static dnode_t *item2dnode(ITEM *item)
+        {
+            return &(item->*DNODE_OFFSET);
+        }
+    };
+
+    template <class ITEM, typename KEY, KEY ITEM::* KEY_OFFSET>
+    class key_is_member {
+    private:
+        key_is_member(const key_is_member &);
+        void operator = (const key_is_member &);
+    public:
+        typedef KEY KEY_TYPE;
+        static void *item2key(ITEM *item)
+        {
+            return &(item->*KEY_OFFSET);
+        }
+    };
+
+    template <typename KEY, int (*COMP)(const KEY &, const KEY &)>
+    class compare_with_function {
+    private:
+        compare_with_function(const compare_with_function &);
+        void operator = (const compare_with_function &);
+    public:
+        static int compare(const void *left, const void *right)
+        {
+            return COMP(*(const KEY *) left, *(const KEY *) right);
+        }
+    };
+
+    template <class ITEM>
+    class dnode_is_base {
+    private:
+        dnode_is_base(const dnode_is_base &);
+        void operator = (const dnode_is_base &);
+    public:
+        typedef ITEM ITEM_TYPE;
+        static ITEM *dnode2item(dnode_t *node)
+        {
+            return static_cast<ITEM *>(node);
+        }
+        static dnode_t *item2dnode(ITEM *item)
+        {
+            return item;
+        }
+    };
+
+    template <class ITEM>
+    class key_is_base {
+    private:
+        key_is_base(const key_is_base &);
+        void operator = (const key_is_base &);
+    public:
+        typedef ITEM KEY_TYPE;
+        static void *item2key(ITEM *item)
+        {
+            return item;
+        }
+    };
+
+    template <
+        class ITEM,
+        typename KEY,
+        dnode ITEM::* DNODE_OFFSET,
+        KEY ITEM::* KEY_OFFSET,
+        bool ALLOW_DUPES = false,
+        int (*COMP)(const KEY &, const KEY &) = default_compare
+    >
+    class dict : public dict_base<dnode_is_member<ITEM, DNODE_OFFSET>,
+                                  key_is_member<ITEM, KEY, KEY_OFFSET>,
+                                  compare_with_function<KEY, COMP>,
+                                  ALLOW_DUPES>
+    {
+    private:
+        dict(const dict &);
+        void operator = (const dict &);
+    public:
+        dict(dictcount_t dict_count = DICTCOUNT_T_MAX)
+        : dict_base<dnode_is_member<ITEM, DNODE_OFFSET>,
+                    key_is_member<ITEM, KEY, KEY_OFFSET>,
+                    compare_with_function<KEY, COMP>,
+                    ALLOW_DUPES>(dict_count)
+        {
+        }
+    };
+
+    template <
+        class ITEM,
+        bool ALLOW_DUPES = false,
+        int (*COMP)(const ITEM &, const ITEM &) = default_compare
+    >
+    class dict_bdbk : public dict_base<dnode_is_base<ITEM>,
+                                       key_is_base<ITEM>,
+                                       compare_with_function<ITEM, COMP>,
+                                       ALLOW_DUPES>
+    {
+    private:
+        dict_bdbk(const dict_bdbk &);
+        void operator = (const dict_bdbk &);
+    public:
+        dict_bdbk(dictcount_t dict_count = DICTCOUNT_T_MAX)
+        : dict_base<dnode_is_base<ITEM>,
+                    key_is_base<ITEM>,
+                    compare_with_function<ITEM, COMP>,
+                    ALLOW_DUPES>(dict_count)
+        {
+        }
+    };
+
+    template <
+        class ITEM,
+        typename KEY,
+        KEY ITEM::* KEY_OFFSET,
+        bool ALLOW_DUPES = false,
+        int (*COMP)(const KEY &, const KEY &) = default_compare
+    >
+    class dict_bdmk : public dict_base<dnode_is_base<ITEM>,
+                                       key_is_member<ITEM, KEY, KEY_OFFSET>,
+                                       compare_with_function<ITEM, COMP>,
+                                       ALLOW_DUPES>
+    {
+        dict_bdmk(const dict_bdmk &);
+        void operator = (const dict_bdmk &);
+    private:
+        dict_bdmk(dictcount_t dict_count = DICTCOUNT_T_MAX)
+        : dict_base<dnode_is_base<ITEM>,
+                    key_is_member<ITEM, KEY, KEY_OFFSET>,
+                    compare_with_function<ITEM, COMP>,
+                    ALLOW_DUPES>(dict_count)
+        {
+        }
+    };
+
+    template <
+        class ITEM,
+        typename KEY,
+        dnode ITEM::* DNODE_OFFSET,
+        bool ALLOW_DUPES = false,
+        int (*COMP)(const ITEM &, const ITEM &) = default_compare
+    >
+    class dict_mdbk : public dict_base<dnode_is_member<ITEM, DNODE_OFFSET>,
+                                       key_is_base<ITEM>,
+                                       compare_with_function<ITEM, COMP>,
+                                       ALLOW_DUPES>
+    {
+        dict_mdbk(const dict_mdbk &);
+        void operator = (const dict_mdbk &);
+    private:
+        dict_mdbk(dictcount_t dict_count = DICTCOUNT_T_MAX)
+        : dict_base<dnode_is_member<ITEM, DNODE_OFFSET>,
+                    key_is_base<ITEM>,
+                    compare_with_function<ITEM, COMP>,
+                    ALLOW_DUPES>(dict_count)
+        {
+        }
+    };
+
+    template <
+        class ITEM,
+        typename KEY,
+        dnode ITEM::* DNODE_OFFSET,
+        KEY ITEM::* KEY_OFFSET,
+        bool ALLOW_DUPES = false,
+        int (*COMP)(const KEY &, const KEY &) = default_compare
+    >
+    class dict_mdmk : public dict_base<dnode_is_member<ITEM, DNODE_OFFSET>,
+                                       key_is_member<ITEM, KEY, KEY_OFFSET>,
+                                       compare_with_function<KEY, COMP>,
+                                       ALLOW_DUPES>
+    {
+    private:
+        dict_mdmk(const dict_mdmk &);
+        void operator = (const dict_mdmk &);
+    public:
+        dict_mdmk(dictcount_t dict_count = DICTCOUNT_T_MAX)
+        : dict_base<dnode_is_member<ITEM, DNODE_OFFSET>,
+                    key_is_member<ITEM, KEY, KEY_OFFSET>,
+                    compare_with_function<KEY, COMP>,
+                    ALLOW_DUPES>(dict_count)
+        {
+        }
+    };
+
+
+}
+
 #endif
 
 #endif
